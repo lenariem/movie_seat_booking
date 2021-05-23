f3de7d58342d13d7e7537cf9f78c2bd81a00d620
unknown <olena.denysova88@gmail.com>
2021-05-23 16:08:59 +0200

    script

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-// dark/light mode
+const btn = document.getElementById('mode');
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+
+// Dark/Light mode
 function toggleMode() {
     const element = document.body;
     element.classList.toggle('light-mode');
 }
 
-const btn = document.getElementById('mode');
 btn.addEventListener('click', toggleMode);
+
+//Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//Movie select event
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+
+//Seat click event
+container.addEventListener('click', (e) => {
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+})
+
