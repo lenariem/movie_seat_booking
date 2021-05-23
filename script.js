0a9e295882d49127293997d03055310491e32503
unknown <olena.denysova88@gmail.com>
2021-05-23 16:59:15 +0200

    local storage movie,seats,price

--- script.js
+++ script.js
@@ -16,9 +16,23 @@ function toggleMode() {
 
 btn.addEventListener('click', toggleMode);
 
+//Save selected movie and price to LocalStorage
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
 //Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat =>
+        [...seats].indexOf(seat)
+    );
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
@@ -28,6 +42,7 @@ function updateSelectedCount() {
 //Movie select event
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
